71465536f5717bdd9752348911af2fe915e432a7
Baris Ceviz <me@barisceviz.com>
2019-04-28 19:15:43 +0300

    Add Dependency Injection

---
+++ src/Microsoft.Extensions.AzureStorage/DependencyInjectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.Extensions.AzureStorage
+{
+    public static class DependencyInjectionExtensions
+    {
+        public static IServiceCollection AddAzureStorage(this IServiceCollection services, AzureStorageOptions option)
+        {
+            services.AddScoped<IBlobProvider>(provider => new BlobProvider(option));
+            return services;
+        }
+    }
+}
