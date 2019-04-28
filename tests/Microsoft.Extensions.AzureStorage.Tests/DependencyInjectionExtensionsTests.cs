eae9f42ae40d4e4ee5aa2ad7b5fc2f702511007f
Baris Ceviz <me@barisceviz.com>
2019-04-28 19:15:51 +0300

    Implement Test project

---
+++ tests/Microsoft.Extensions.AzureStorage.Tests/DependencyInjectionExtensionsTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Microsoft.Extensions.AzureStorage.Tests
+{
+    public class DependencyInjectionExtensionsTests
+    {
+        [Fact]
+        public void It_should_add_service()
+        {
+            //Arrange
+            var services = new ServiceCollection();
+            var option = new AzureStorageOptions()
+            {
+
+            };
+
+            //Act
+            services.AddAzureStorage(option);
+
+            //Assert
+            services.Should().HaveCount(1);
+        }
+    }
+}
